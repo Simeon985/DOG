ea1aec43dd374d08c88808b2f14636ff369a6354
Simeon Demeester <simeon.demeester@ugent>
2026-02-25 18:36:24 +0100

    Graden uitlezen, kompas (werkt met 0°=initiele richting)

--- ESP32/DOG/src/main.cpp
+++ ESP32/DOG/src/main.cpp
@@ -1,16 +1,4 @@
-/*
-#include <Arduino.h>
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("TEST OK");
-}
-
-void loop() {
-
-  delay(1000);
-}
-*/
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_BNO055.h>
@@ -18,33 +6,28 @@ void loop() {
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   Wire.begin(21, 22);
 
-  if (!bno.begin())
-  {
-    Serial.println("BNO055 NIET GEVONDEN");
-    while (1)
-      ;
+  if (!bno.begin()) {
+    Serial.println("BNO055 niet gevonden");
+    while (1);
   }
 
   delay(1000);
-  Serial.println("BNO055 OK");
+  Serial.println("Kompas OK");
 }
 
-void loop()
-{
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+void loop() {
+  imu::Vector<3> euler =
+      bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-  Serial.print("Yaw: ");
-  Serial.print(euler.x());
-  Serial.print(" Pitch: ");
-  Serial.print(euler.y());
-  Serial.print(" Roll: ");
-  Serial.println(euler.z());
-  Serial.println("Hallo");
+  float heading = euler.x();   // kompasrichting
+
+  Serial.print("Heading: ");
+  Serial.print(heading);
+  Serial.println(" graden");
+
   delay(500);
-}
-  
+  }
