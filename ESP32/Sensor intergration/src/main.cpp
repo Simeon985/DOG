000f0d213298b29525e7258c0bb848826d3eb022
Simeon Demeester <simeon.demeester@ugent>
2026-03-04 19:19:16 +0100

    Data_capture_1_slecht_wrs

--- ESP32/Sensor intergration/src/main.cpp
+++ ESP32/Sensor intergration/src/main.cpp
@@ -5,10 +5,11 @@
 #include <Adafruit_BNO055.h>
 #include <Adafruit_Sensor.h>
 
-#define PIN_SCK_OFS_1  18
-#define PIN_MISO_OFS_1 19
-#define PIN_MOSI_OFS_1 23
+#define PIN_SCK_OFS  18
+#define PIN_MISO_OFS 19
+#define PIN_MOSI_OFS 23
 #define PIN_CS_OFS_1   5
+#define PIN_CS_OFS_2   14
 #define PIN_TRIG_US_1 17
 #define PIN_ECHO_US_1 16
 #define PIN_SDA_IMU 21
@@ -16,7 +17,8 @@
 #define IMU_SENSOR_ID 55
 
 //put function declarations here:
-Optical_Flow_Sensor flow(PIN_SCK_OFS_1, PIN_MISO_OFS_1, PIN_MOSI_OFS_1, PIN_CS_OFS_1, PAA5100);
+Optical_Flow_Sensor flow1(PIN_SCK_OFS, PIN_MISO_OFS, PIN_MOSI_OFS, PIN_CS_OFS_1, PAA5100);
+Optical_Flow_Sensor flow2(PIN_SCK_OFS, PIN_MISO_OFS, PIN_MOSI_OFS, PIN_CS_OFS_2, PAA5100);
 Ultrasone_sensor ultra(PIN_TRIG_US_1, PIN_ECHO_US_1);
 Adafruit_BNO055 bno = Adafruit_BNO055(IMU_SENSOR_ID);
 hw_timer_t *timer = NULL;
@@ -27,11 +29,16 @@ void IRAM_ATTR onTimer() {
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(921600);
   delay(1000);
-  if (!flow.begin()) {
+  if (!flow1.begin()) {
     while(true){
-      Serial.println("Initialization of the flow sensor failed");
+      Serial.println("Initialization of the flow sensor 1 failed");
+    }
+  }
+  if (!flow2.begin()) {
+    while(true){
+      Serial.println("Initialization of the flow sensor 2 failed");
     }
   }
   ultra.begin();
@@ -45,11 +52,13 @@ void setup() {
   timer = timerBegin(0, 80, true);  // 80MHz / 80 = 1MHz (1 tick = 1µs)
   timerAttachInterrupt(timer, &onTimer, true);
   // timerAlarmWrite(timer, ticks, repeat)
-  timerAlarmWrite(timer, 1000000, true);  // 1/10 000 µs = 100 Hz
+  timerAlarmWrite(timer, 100000, true);  // 1/10 000 µs = 100 Hz
   timerAlarmEnable(timer);
+  Serial.println("Timing flow\n");
 }
 
-int16_t deltaX,deltaY;
+int16_t deltaX1, deltaY1;
+int16_t deltaX2, deltaY2;
 unsigned long previous_time = micros();
 unsigned long current_time;
 float distance;
@@ -68,7 +77,8 @@ void loop() {
     timerFired = false;
 
   // reading data optical flow sensors
-  flow.readMotionCount(&deltaX, &deltaY);
+  flow1.readMotionCount(&deltaX1, &deltaY1);
+  flow2.readMotionCount(&deltaX2, &deltaY2);
 
   // reading data ultrasone sensors
   ultra.read_distance(distance);
@@ -78,7 +88,7 @@ void loop() {
   gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   lin_acc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
   heading = euler.x();
-  gyro_x = gyro.x();
+  gyro_x = gyro.z();
   lin_acc_x = lin_acc.x();
   lin_acc_y = lin_acc.y();
 
@@ -90,7 +100,7 @@ void loop() {
   // Serial.print(", Y: ");
   // Serial.print(deltaY);
   // Serial.print(", time:");
-  // current_time = micros();
+  current_time = micros();
   // Serial.print(current_time - previous_time);
   // Serial.print("micros \n");
 
@@ -99,19 +109,26 @@ void loop() {
   // Serial.println(distance);
 
   //data IMU
-  Serial.print("Heading: ");
+  //print everything in one line
+  Serial.print(current_time-previous_time);
+  Serial.print(" ");
   Serial.print(heading);
-  Serial.println("°");
-  Serial.print("Gyro: ");
-  Serial.println(gyro_x);
-  Serial.print("linear acceleration in x direction: ");
+  Serial.print(" ");
+  Serial.print(gyro_x);
+  Serial.print("   ");
   Serial.print(lin_acc_x);
-  Serial.println("m/s²");
-  Serial.print("linear acceleration in y direction: ");
+  Serial.print(" ");
   Serial.print(lin_acc_y);
-  Serial.println("m/s²");
-
-
+  Serial.print("   ");
+  Serial.print(deltaX1);
+  Serial.print(" ");
+  Serial.print(deltaY1);
+  Serial.print("   ");
+  Serial.print(deltaX2);
+  Serial.print(" ");
+  Serial.print(deltaY2);
+  Serial.print("   ");
+  Serial.println(distance);
   previous_time = current_time;
   }
 }
