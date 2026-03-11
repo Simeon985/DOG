721fe6a0e7facc319ecc17ee39f04455a8370c71
Andreas Van Bockhaven <Andreas.vanbockhaven@ugent.be>
2026-03-11 19:33:30 +0100

    LED matrix Driver

--- ESP32/Sensor intergration/src/main.cpp
+++ ESP32/Sensor intergration/src/main.cpp
@@ -21,15 +21,15 @@
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES	2
-#define CLK_PIN_LED   34  // or SCK
+#define CLK_PIN_LED   25  // or SCK
 #define DATA_PIN_LED  32  // or MOSI
-#define CS_PIN_LED    35  // or SS
+#define CS_PIN_LED    26  // or SS
 
 //put function declarations here:
-Optical_Flow_Sensor flow1(PIN_SCK_OFS, PIN_MISO_OFS, PIN_MOSI_OFS, PIN_CS_OFS_1, PAA5100);
-Optical_Flow_Sensor flow2(PIN_SCK_OFS, PIN_MISO_OFS, PIN_MOSI_OFS, PIN_CS_OFS_2, PAA5100);
-Ultrasone_sensor ultra(PIN_TRIG_US_1, PIN_ECHO_US_1);
-Adafruit_BNO055 bno = Adafruit_BNO055(IMU_SENSOR_ID);
+// Optical_Flow_Sensor flow1(PIN_SCK_OFS, PIN_MISO_OFS, PIN_MOSI_OFS, PIN_CS_OFS_1, PAA5100);
+// Optical_Flow_Sensor flow2(PIN_SCK_OFS, PIN_MISO_OFS, PIN_MOSI_OFS, PIN_CS_OFS_2, PAA5100);
+// Ultrasone_sensor ultra(PIN_TRIG_US_1, PIN_ECHO_US_1);
+// Adafruit_BNO055 bno = Adafruit_BNO055(IMU_SENSOR_ID);
 hw_timer_t *timer = NULL;
 volatile bool timerFired = false;
 void IRAM_ATTR onTimer() {
@@ -41,23 +41,23 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(921600);
   delay(1000);
-  if (!flow1.begin()) {
-    while(true){
-      Serial.println("Initialization of the flow sensor 1 failed");
-    }
-  }
-  if (!flow2.begin()) {
-    while(true){
-      Serial.println("Initialization of the flow sensor 2 failed");
-    }
-  }
-  ultra.begin();
-  Wire.begin(PIN_SDA_IMU, PIN_SCL_IMU);
-  if (!bno.begin()) {
-    Serial.println("BNO055 niet gevonden");
-    while (1);
-  }
-  bno.setExtCrystalUse(true);
+  // if (!flow1.begin()) {
+  //   while(true){
+  //     Serial.println("Initialization of the flow sensor 1 failed");
+  //   }
+  // }
+  // if (!flow2.begin()) {
+  //   while(true){
+  //     Serial.println("Initialization of the flow sensor 2 failed");
+  //   }
+  // }
+  // ultra.begin();
+  // Wire.begin(PIN_SDA_IMU, PIN_SCL_IMU);
+  // if (!bno.begin()) {
+  //   Serial.println("BNO055 niet gevonden");
+  //   while (1);
+  // }
+  // bno.setExtCrystalUse(true);
   // timerBegin(timer number, prescaler, count up)
   timer = timerBegin(0, 80, true);  // 80MHz / 80 = 1MHz (1 tick = 1µs)
   timerAttachInterrupt(timer, &onTimer, true);
@@ -93,20 +93,20 @@ void loop() {
     timerFired = false;
 
   // reading data optical flow sensors
-  flow1.readMotionCount(&deltaX1, &deltaY1);
-  flow2.readMotionCount(&deltaX2, &deltaY2);
+  // flow1.readMotionCount(&deltaX1, &deltaY1);
+  // flow2.readMotionCount(&deltaX2, &deltaY2);
 
-    // reading data ultrasone sensors
-    ultra.read_distance(distance);
+  //   // reading data ultrasone sensors
+  //   ultra.read_distance(distance);
 
-  //reading data IMU
-  euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  lin_acc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  heading = euler.x();
-  gyro_x = gyro.z();
-  lin_acc_x = lin_acc.x();
-  lin_acc_y = lin_acc.y();
+  // //reading data IMU
+  // euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  // gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  // lin_acc = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  // heading = euler.x();
+  // gyro_x = gyro.z();
+  // lin_acc_x = lin_acc.x();
+  // lin_acc_y = lin_acc.y();
 
     // printing all the data
 
@@ -119,36 +119,33 @@ void loop() {
     //Serial.println(distance);
 
     //driver for the LED screens
-    for (u_int8_t i=0; i < 3; i++){
-      mx.setRow(i, 32);
-      mx.setRow(7-i, 32);
-    }
     for (u_int8_t i = 3; i < 5; i++){
       mx.setRow(i, 255);
+      mx.setColumn(i, 255);
     }
 
   //data IMU
   //print everything in one line
-  Serial.print(current_time-previous_time);
-  Serial.print(" ");
-  Serial.print(heading);
-  Serial.print(" ");
-  Serial.print(gyro_x);
-  Serial.print("   ");
-  Serial.print(lin_acc_x);
-  Serial.print(" ");
-  Serial.print(lin_acc_y);
-  Serial.print("   ");
-  Serial.print(deltaX1);
-  Serial.print(" ");
-  Serial.print(deltaY1);
-  Serial.print("   ");
-  Serial.print(deltaX2);
-  Serial.print(" ");
-  Serial.print(deltaY2);
-  Serial.print("   ");
-  Serial.println(distance);
-  previous_time = current_time;
+  // Serial.print(current_time-previous_time);
+  // Serial.print(" ");
+  // Serial.print(heading);
+  // Serial.print(" ");
+  // Serial.print(gyro_x);
+  // Serial.print("   ");
+  // Serial.print(lin_acc_x);
+  // Serial.print(" ");
+  // Serial.print(lin_acc_y);
+  // Serial.print("   ");
+  // Serial.print(deltaX1);
+  // Serial.print(" ");
+  // Serial.print(deltaY1);
+  // Serial.print("   ");
+  // Serial.print(deltaX2);
+  // Serial.print(" ");
+  // Serial.print(deltaY2);
+  // Serial.print("   ");
+  // Serial.println(distance);
+  // previous_time = current_time;
   }
 }
 
