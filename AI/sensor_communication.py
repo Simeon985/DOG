18531755beca728cf0c2ff81c543c76fd951fa9b
Andreas Van Bockhaven <Andreas.vanbockhaven@ugent.be>
2026-03-24 18:56:02 +0100

    Sensor communication done

---
+++ AI/sensor_communication.py
@@ -0,0 +1,30 @@
+import serial
+from serial.tools.list_ports import comports
+import numpy as np
+
+def initialize_esp(baudrate=921600):
+    #standard Espressif vendor id's
+    ESP_VIDS = {0x10C4, 0x1A86}
+
+    #find the port the ESP is connected to
+    esp_ports = [p for p in comports() if p.vid in ESP_VIDS]
+
+    if len(esp_ports) == 0:
+        raise RuntimeError("No ESP connection found")
+    if len(esp_ports) > 1:
+        raise RuntimeError(f"Multiple ESPs connected ({len(esp_ports)} found), expected exactly one")
+
+    ser = serial.Serial(esp_ports[0].device, baudrate=baudrate)
+    ser.read()  # wait for connection
+    return ser
+
+def get_sensor_data(ser: serial.Serial):
+    data_array = np.zeros(10)
+    data = ser.readline()
+    data_list = data.split()
+    for i in range(5):
+        data_array[i] = float(data_list)      
+    for i in range(5,10):
+        data_array[i] = int(data_list)
+    return data_array
+
